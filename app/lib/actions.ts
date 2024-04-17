290fc645f35f4a54f23dbad342fc7c09f184e0ab
BobbiePaulsen <paulsenbobbie1@gmail.com>
2024-04-17 13:43:46 +1200

    Added try/catch to the server actions for invoices.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,16 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function deleteInvoice(id: string) {
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');    
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+  }
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -23,12 +33,40 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+  try {
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Create Invoice.' };
+  }
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;
 
 revalidatePath('/dashboard/invoices');
 redirect('/dashboard/invoices');
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   try {
+       await sql`
+         UPDATE invoices
+         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+         WHERE id = ${id}
+       `;
+   } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+   }
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+  
